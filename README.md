ef7c480624d9f464503d3821bb0fc154a537ee0c
Pottery_design <jackie.lu2@Education.nsw.gov.au>
2026-08-14 13:21:19 +1000

    Basic spaceship movements and laser firing all done.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Space-Invaders-Yr9
 This is an assessment task for Computer Technology, Term 3.
+
+Brief Overview
+
+Setup Instructions
+
+How To Run The Game
---
+++ laser.py
@@ -0,0 +1,17 @@
+import pygame
+
+# Creating Lasers (Both Player and Enemy)
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, position, speed, screen_height):
+        super().__init__()
+        self.image = pygame.Surface((4, 15))
+        self.image.fill((243, 216, 63))
+        self.rect = self.image.get_rect(center = position)
+        self.speed = speed
+        self.screen_height = screen_height
+
+    def update(self):
+        self.rect.y -= self.speed
+        # Destroying Lasers If Not In Game Window
+        if self.rect.y > self.screen_height + 15 or self.rect.y < 0:
+            self.kill()
