fb926d632329de31d9f5f081c468013087578d0e
Pottery-design <jackie.lu2@Education.nsw.gov.au>
2026-08-18 11:30:08 +1000

    Add files via upload
    This is work of about a hour into the Spaceship Video.

---
+++ main.py
@@ -0,0 +1,44 @@
+# 1:21:04
+
+import pygame, sys
+from game import Game
+# Variables
+SCREEN_WIDTH = 750
+SCREEN_HEIGHT = 700
+
+GREY = (29, 29, 27)
+
+# Basic Screen Display & Set Up
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Python Space Invaders")
+
+clock = pygame.time.Clock()
+
+game = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# Game Loop / Updating
+while True:
+    # Checking for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Updating
+    game.spaceship_group.update()
+    game.move_aliens()
+    game.alien_shoot_laser()
+    game.alien_lasers_group.update()
+
+    # Drawing
+    screen.fill(GREY)
+    game.spaceship_group.draw(screen)
+    game.spaceship_group.sprite.lasers_group.draw(screen)
+    for obstacle in game.obstacles:
+        obstacle.blocks_group.draw(screen)
+    game.aliens_group.draw(screen)
+    game.alien_lasers_group.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
---
+++ obstacle.py
@@ -0,0 +1,41 @@
+import pygame
+
+# Creating Individual Blocks
+class Block(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((3,3))
+        self.image.fill((243, 216, 63))
+        self.rect = self.image.get_rect(topleft = (x, y))
+
+# Grid System
+grid = [
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]]
+# NOTE: In this 13 by 23 grid, each value represents a 3x3 pixel.
+
+
+# Container for Multiple Blocks
+class Obstacle:
+    def __init__(self, x, y):
+        self.blocks_group = pygame.sprite.Group()
+        # Checks Each Row
+        for row in range(len(grid)): 
+            # Checks Each Column
+            for column in range(len(grid[0])):
+                if grid[row][column] == 1:
+                    pos_x = x + column * 3
+                    pos_y = y + row * 3
+                    block = Block(pos_x, pos_y)
+                    self.blocks_group.add(block)
